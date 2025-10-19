1779f0a691cf948d919d7846c57e98a7e9be11ef
Aditya <adityaalt9090@gmail.com>
2025-10-19 11:00:18 +0530

    Seperate Script.js added

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Ways to print in Javascript
+// console.log("Hello world",10); // normal
+// alert("me");
+// document.write("this is a website");
+//2. JAVASCRIPT console API
+console.warn("this is a very serious problem"); // warns and yellow in color
+console.error("This is an error"); // serious red in color
+console.assert(4==(6-2)); // used for assertion to check things
+
+// 3. JavaScript Variables
