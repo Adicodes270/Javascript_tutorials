f034d508ce035fc591c62fdb51adebaa52b21abf
Aditya <adityaalt9090@gmail.com>
2025-10-19 14:43:18 +0530

    Functions and Operators studied

--- index.js
+++ index.js
@@ -15,42 +15,100 @@ multi
 line 
 comment
 */
-var number = 34;
-var number2 = 56;
-console.log(number + number2);
+// var number = 34;
+// var number2 = 56;
+// console.log(number + number2);
 
-// 4. Data Types in Javascript
-//number
-var num1 = 455;
-var num2=56.76;
+// // 4. Data Types in Javascript
+// //number
+// var num1 = 455;
+// var num2=56.76;
 
-//String
-var str1 ="This is a string";
-var str2 = 'This is another string'
+// //String
+// var str1 ="This is a string";
+// var str2 = 'This is another string'
 
-var marks= {
-    ravi:34,
-    shubham: 78,
-    harry: 99.977
-}
+// var marks= {
+//     ravi:34,
+//     shubham: 78,
+//     harry: 99.977
+// }
 
-console.log(marks);
+// console.log(marks);
 
 // Booleans
-var a = true;
-var b = false;
-console.log(a , b);
+// var a = true;
+// var b = false;
+// console.log(a , b);
 
-// var und = undefined;
-var und;
-console.log(und);
+// // var und = undefined;
+// var und;
+// console.log(und);
 
-var n = null;
-console.log(n);
+// var n = null;
+// console.log(n);
 
 // at a very high level, there are two types of data types in Javascript
 // 1. Primitive : undefined,null,number,number,string,boolean,symbol
 // 2. Reference : Arrays and Objects
 
-var arr= [1,2,'string',4,5]
-console.log(arr);
+// var arr= [1,2,'string',4,5]
+// console.log(arr);
+
+// Opeators in Java
+// Arthimetic Operators
+// var a = 100;
+// var b = 10;
+// console.log("The value of a + b is ",a+b);
+// console.log("The value of a - b is ",a-b);
+// console.log("The value of a * b is ",a*b);
+// console.log("The value of a / b is ",a/b);
+
+// Assignment Operators
+// var c = b;
+// c += 2;
+// c /= 2;
+// c *= 2;
+// c -= 2;
+// console.log(c);
+
+// // Comparision Operators
+// var x= 34;
+// var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
+
+// Logical Operators
+
+// console.log(true && true); // All are false except true and true
+// console.log(true && false);
+// console.log(false && false);
+// console.log(false && true);
+
+
+// console.log(true || true); // All are true except false and false
+// console.log(true || false);
+// console.log(false || false);
+// console.log(false || true);
+
+// Logical not
+
+// console.log(!false);
+// console.log(!true);
+
+// Function in JavaScript
+// DRY = DO NOT REPEAT YOURSELF
+function avg(a,b){
+    c = (a+b)/2;
+    return c;
+}
+
+c = avg(4,6);
+c1 = avg(5,10);
+console.log(c,c1)
+
+// If u see something in blue in console its number
+// If u see something in black in console its string
